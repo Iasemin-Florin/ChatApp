152065971472c56be177b38d5edb5759d0d5c0fd
Iasemin-Florin <queue.triple@gmail.com>
2022-06-20 10:06:06 +0200

    Chat App Basic Setup

--- App.js
+++ App.js
@@ -1,112 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useChatClient } from './useChatClients';
+import { Channel, Chat, OverlayProvider } from 'stream-chat-react-native';
+import { StreamChat } from 'stream-chat';
+import { ChannelList, MessageList, MessageInput } from 'stream-chat-react-native';
+import { chatApiKey, chatUserId } from './chatConfig';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createStackNavigator();
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const filters = {
+  members: {
+    '$in': [chatUserId]
+  }
+}
+
+const sort = {
+  last_message_at: -1 
+};
+
+const ChannelScreen = props => {
+  const { route } = props;
+  const { params: { channel } } = route;
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <Channel channel={channel}>
+      <MessageList />
+      <MessageInput />
+    </Channel>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const ChannelListScreen = props => {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <ChannelList
+      onSelect={(channel) => {
+      const { navigation } = props;
+      navigation.navigate('ChannelScreen', { channel });
+    }}
+    />
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const chatClient = StreamChat.getInstance(chatApiKey);
 
-export default App;
+const NavigationStack = () => {
+  const { clientIsReady } = useChatClient();
+
+  if (!clientIsReady) {
+    return <Text>Loading Chat...</Text>
+  }
+  
+  return (
+    <OverlayProvider>
+      <Chat client={chatClient}>
+      <Stack.Navigator>
+        <Stack.Screen name="Groups" component={ChannelListScreen} />
+        <Stack.Screen name="ChannelScreen" component={ChannelScreen} />
+      </Stack.Navigator>
+
+      </Chat>
+    </OverlayProvider>
+    
+  );
+};
+
+export default () => {
+  
+  return (
+    <GestureHandlerRootView style={{ flex: 1}}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <NavigationStack />
+        </NavigationContainer>
+      </SafeAreaView>
+    </GestureHandlerRootView>
+  );
+};
---
+++ chatConfig.js
@@ -0,0 +1,11 @@
+/*export const chatApiKey = 'zhbvg2wpx3ft';
+export const chatUserId = 'iasemin-florin';
+export const chatUserToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaWFzZW1pbi1mbG9yaW4ifQ.K07DBTrZ2L7sIpgoOaoh6dVtcrgSINhOjKrd-__nItE';
+export const chatUserName = 'iasemin-florin';   
+*/
+
+export const chatApiKey = 'zhbvg2wpx3ft';
+export const chatUserId = 'Lazzy';
+export const chatUserToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiTGF6enkifQ.SFfDWdZfPq-g-oh_Mkc8m6uwEFL8VViQqA-7qmXIuCs';
+export const chatUserName = 'Lazzy';  
+
